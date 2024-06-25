b52e9b65295a591651dad42717573ebd6c0e3072
mjedryczka <jedryczkamaciej@gmail.com>
2024-06-25 15:11:02 -0400

    Finished adding logging

--- app/plugins/exit/__init__.py
+++ app/plugins/exit/__init__.py
@@ -1,7 +1,9 @@
 import sys
 from app.commands import Command
+import logging
 
 
 class ExitCommand(Command):
     def execute(self):
+        logging.info("Exit command executing")
         sys.exit("Exiting...")
--- app/plugins/multiply/__init__.py
+++ app/plugins/multiply/__init__.py
@@ -2,9 +2,11 @@ from decimal import Decimal
 from calculator.calculation import Calculation
 from calculator.operations import multiply
 from app.commands import Command
-
+import logging
 
 class MultiplyCommand(Command):
     def execute(self, numberA: str, numberB: str):
         calculation = Calculation(Decimal(numberA), Decimal(numberB), multiply)
-        print(calculation.perform())
+        result = calculation.perform()
+        logging.info(result)
+        print(result)
